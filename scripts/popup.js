ab9e9ea2eb8aa56dbc6d7a2014ffb6eda1f10a89
yuseon.lim <dogakday@gmail.com>
2024-01-22 20:19:54 +0900

    feat: get OAuth token
    - 버튼 클릭 시 Google OAuth 통한 인증 토큰 가져오는 기능 구현
    -  공유받은 key 개발용 확장프로그램에 적용 안되어 clientID 일단 비워둠
    
    [#2]

---
+++ scripts/popup.js
@@ -0,0 +1,14 @@
+const signWithGooleBtn = document.getElementById("signWithGoogleBtn");
+
+/**
+ * get google OAuth2 token
+ */
+signWithGooleBtn.addEventListener("click", () => {
+  chrome.identity.getAuthToken({ interactive: true }, (token) => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+    console.log("Obtained token:", token);
+  });
+});
