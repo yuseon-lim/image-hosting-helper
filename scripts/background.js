07d2940ffbbc5360f4be85ff7c1d7300640d74e8
yuseon.lim <dogakday@gmail.com>
2024-01-24 01:33:32 +0900

    feat: get userInfo using Google Drive API
    [#2]

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,30 @@
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  switch (request.action) {
+    case "getUserInfo":
+      const userInfo = await getUserInfo(request.token);
+      console.log(userInfo);
+      break;
+    default:
+      console.error("unknown request.");
+  }
+});
 
+const getUserInfo = async (token) => {
+  const response = await fetch(
+    "https://www.googleapis.com/drive/v3/about?fields=user",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+      },
+    }
+  );
+
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    // TODO: exception 처리
+    console.error(response);
+  }
+};
